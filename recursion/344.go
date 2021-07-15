c11e232623e91401199b8ef6740545a3632b8675
康茜 <xikang2@creditease.cn>
2021-07-15 22:25:02 +0800

    add recursion module

---
+++ recursion/344.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+//编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+//
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+//
+// 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+//
+//
+//
+// 示例 1：
+//
+// 输入：["h","e","l","l","o"]
+//输出：["o","l","l","e","h"]
+//
+//
+// 示例 2：
+//
+// 输入：["H","a","n","n","a","h"]
+//输出：["h","a","n","n","a","H"]
+// Related Topics 递归 双指针 字符串
+// 👍 423 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func reverseString(s []byte) {
+	ret := make([]byte, 0)
+	reverse(s, 0, &ret)
+	for i := 0; i < len(s); i++ {
+		s[i] = ret[i]
+	}
+}
+
+func reverse(s []byte, i int, ret *[]byte) {
+	if i == len(s) {
+		return
+	}
+
+	reverse(s, i+1, ret)
+	*ret = append(*ret, s[i])
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+func main() {
+	chars := []byte{'a', 'b', 'c'}
+	fmt.Printf("%s\n", chars)
+	reverseString(chars)
+	fmt.Printf("%s\n", chars)
+}
