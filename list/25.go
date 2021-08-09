7d8ba97e82fb29bb2e4260e6979926efd22a1940
康茜 <xikang2@creditease.cn>
2021-08-09 19:25:15 +0800

    add reverse list pre k

---
+++ list/25.go
@@ -0,0 +1,102 @@
+package main
+
+//给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+//
+// k 是一个正整数，它的值小于或等于链表的长度。
+//
+// 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+//
+// 进阶：
+//
+//
+// 你可以设计一个只使用常数额外空间的算法来解决此问题吗？
+// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,3,4,5], k = 2
+//输出：[2,1,4,3,5]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [1,2,3,4,5], k = 3
+//输出：[3,2,1,4,5]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = [1,2,3,4,5], k = 1
+//输出：[1,2,3,4,5]
+//
+//
+// 示例 4：
+//
+//
+//输入：head = [1], k = 1
+//输出：[1]
+//
+//
+//
+//
+//
+// 提示：
+//
+//
+// 列表中节点的数量在范围 sz 内
+// 1 <= sz <= 5000
+// 0 <= Node.val <= 1000
+// 1 <= k <= sz
+//
+// Related Topics 递归 链表
+// 👍 1232 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+//思路：k个一组翻转链表，链表长度小于k个，不做处理。 使用递归思路，将链表分成两部分（前k个 和 后l-k个），先翻转前k个，然后递归翻转剩余的部分
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	dummy := &ListNode{Next: head}
+	end := dummy
+
+	for i := 0; i < k && end != nil; i++ {
+		end = end.Next
+	}
+	//长度不足k个
+	if end == nil {
+		return head
+	}
+	//split
+	next := end.Next
+	end.Next = nil
+
+	dummy.Next = reverseListV5(head)
+	head.Next = reverseKGroup(next, k)
+
+	return dummy.Next
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+func reverseListV5(head *ListNode) *ListNode {
+	var pre, next *ListNode
+	cur := head
+	for cur != nil {
+		next = cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
